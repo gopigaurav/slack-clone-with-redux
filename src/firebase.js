d454e819edd2ce27dcfb8ecf81383e586647fc21
gopi <48626860+gopigaurav@users.noreply.github.com>
2022-08-08 21:46:30 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,6 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCNslLwus72iQxrPxweT2Xe3izqASxkF50",
-  authDomain: "slack-clone-redux-b0b28.firebaseapp.com",
-  projectId: "slack-clone-redux-b0b28",
-  storageBucket: "slack-clone-redux-b0b28.appspot.com",
-  messagingSenderId: "626602935886",
-  appId: "1:626602935886:web:3552b36507ae6441cee51f",
-  measurementId: "G-2P74GBH5VB",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
